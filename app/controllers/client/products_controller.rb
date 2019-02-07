32368c335b0353c7761787a1f543f85e10f40376
pattywitwhy <pattyxhwang@gmail.com>
2019-02-07 12:44:58 -0600

    new actions

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,7 +1,33 @@
 class Client::ProductsController < ApplicationController
   def index
     response = HTTP.get("http://localhost:3000/api/products")
-    @product = response.parse
+    @products = response.parse
     render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                      name: params[:name],
+                      price: params[:price],
+                      description: params[:description],
+                      supplier_id: params[:supplier_id]
+                      # image_url: params[:image_url]
+                    }
+    response = HTTP.post(
+                        "http://localhost:3000/api/products",
+                        form: client_params
+                      )
+    product = response.parse
+    redirect_to "/client/products/#{ product["id"] }"
+  end
+
+  def show
+    response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
+    @product = response.parse
+    render 'show.html.erb'
   end
 end
